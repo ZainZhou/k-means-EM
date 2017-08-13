e0dc0b78e66bce5b2229b7f4b2a453692ee44c21
T&H <truemenhale@gmail.com>
2017-08-14 00:31:00 +0800

    over

--- em.py
+++ em.py
@@ -4,29 +4,34 @@ import numpy as np
 import math  
 import copy  
 import matplotlib.pyplot as plt  
+import matplotlib.mlab as mlab  
 
 isdebug = True
 
 # 指定k个高斯分布参数，这里指定k=2。注意2个高斯分布具有相同均标准差Sigma，均值分别为Mu1,Mu2。  
-def init_data(Sigma,Mu1,Mu2,k,N):  
+def InitData(Sigma,mu_r,k,N):  
 	global X  
 	global Mu  
 	global Expectations  
+	global guass_real
+	guass_real = [[] for i in range(k)]
 	X = np.zeros((1,N))  
 	Mu = np.random.random(k)
 	Expectations = np.zeros((N,k))  
 	for i in range(0,N):  
 		if np.random.random(1) > 0.5:  
-			X[0,i] = np.random.normal(Mu1, Sigma)
+			X[0,i] = np.random.normal(mu_r[0], Sigma)
+			guass_real[0].append(X[0,i])
 		else:  
-			X[0,i] = np.random.normal(Mu2, Sigma)
+			X[0,i] = np.random.normal(mu_r[1], Sigma)
+			guass_real[1].append(X[0,i])
 	if isdebug:  
 		print("***********")
 		print("初始观测数据X：")
 		print(X)
 		
 # EM算法：步骤1，计算E[zij]  
-def e_step(Sigma, k, N):  
+def Estep(Sigma, k, N):  
 	global Expectations  
 	global Mu  
 	global X  
@@ -36,7 +41,7 @@ def e_step(Sigma, k, N):
 		for j in range(0,k):  
 			Numer[j] = math.exp((-1/(2*(float(Sigma**2))))*(float(X[0,i]-Mu[j]))**2)  
 			Denom += Numer[j]
-		for j in range(0,k):  
+		for j in range(0,k): 
 			Expectations[i,j] = Numer[j] / Denom  
 	if isdebug:  
 		print("***********")
@@ -44,38 +49,52 @@ def e_step(Sigma, k, N):
 		print(Expectations)
 		
 # EM算法：步骤2，求最大化E[zij]的参数Mu  
-def m_step(k,N):  
+def Mstep(k,N):  
 	global Expectations  
 	global X  
 	for j in range(0,k):  
 		Numer = 0  
 		Denom = 0  
-		for i in range(0,N):  
+		for i in range(0,N):
 			Numer += Expectations[i,j]*X[0,i]  
-			Denom +=Expectations[i,j]  
+			Denom += Expectations[i,j]
 		Mu[j] = Numer / Denom
 		
 # 算法迭代iter_num次，或达到精度Epsilon停止迭代  
-def run(Sigma,Mu1,Mu2,k,N,iter_num,Epsilon):  
-	init_data(Sigma,Mu1,Mu2,k,N)  
+def run(Sigma,mu_r,k,N,iter_num,Epsilon):  
+	InitData(Sigma,mu_r,k,N)  
 	print("初始<u1,u2>:", Mu)
 	for i in range(iter_num):  
 		Old_Mu = copy.deepcopy(Mu)  
-		e_step(Sigma,k,N)  
-		m_step(k,N)  
-		print(i,Mu)
+		Estep(Sigma,k,N)  
+		Mstep(k, N)
+		print('迭代次数：',i,'μ：',Mu)
 		if sum(abs(Mu - Old_Mu)) < Epsilon:  
 			break  
 
 if __name__ == '__main__':
 	sigma = 6   # 高斯分布具有相同的方差
-	mu1 = 40    # 第一个高斯分布的均值 用于产生样本
-	mu2 = 20    # 第二个高斯分布的均值 用于产生样本
+	mu_r = [40,20]    # 高斯分布的均值 用于产生样本
 	k = 2       # 高斯分布的个数
-	N = 1000    # 样本个数
-	iter_num = 1000 # 最大迭代次数
+	N = 100000    # 样本个数
+	iter_num = 2000 # 最大迭代次数
 	epsilon = 0.0001    # 当两次误差小于这个时退出
-	run(sigma,mu1,mu2,k,N,iter_num,epsilon)  
-   
-	plt.hist(X[0,:],60)
+	run(sigma,mu_r,k,N,iter_num,epsilon)  
+	guass_pre = [[] for i in range(k)]
+	g = []
+	for i in range(len(Expectations)):
+		g_pre = np.where(Expectations[i,:] == max(Expectations[i]))[0][0]
+		guass_pre[g_pre].append(X[0,i])
+	fig,ax = plt.subplots(2,2)
+	for i in range(len(guass_pre)):
+		ax = plt.subplot(2,2,i+3)
+		ax.set_title("Prediction:"+'μ='+str(round(Mu[i],4)))
+		ax.hist(guass_pre[i],1000,facecolor='yellowgreen',alpha=0.75,normed=1)
+		ax.set_xticks(np.linspace(0,60,7))
+	for i in range(len(guass_real)):
+		ax = plt.subplot(2,2,i+1)
+		ax.set_title("Real:"+'μ='+str(round(mu_r[i],4)))
+		ax.hist(guass_real[i],1000,facecolor='red',alpha=0.75,normed=1)
+		ax.set_xticks(np.linspace(0,60,7))
+	fig.subplots_adjust(wspace=0.4,hspace=0.4)
 	plt.show()
